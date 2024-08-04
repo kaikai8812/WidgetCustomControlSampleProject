55e8d6e02274fccbd527cc205a30b2150e9fd960
kaikai8812 <kai.aoyama@dely.jp>
2024-08-04 14:52:24 +0900

    Initial Commit

---
+++ WidgetCustomControlSampleProject/WidgetCustomControlSampleProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetCustomControlSampleProjectApp.swift
+//  WidgetCustomControlSampleProject
+//
+//  Created by aoyamakai on 2024/08/04.
+//
+
+import SwiftUI
+
+@main
+struct WidgetCustomControlSampleProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
